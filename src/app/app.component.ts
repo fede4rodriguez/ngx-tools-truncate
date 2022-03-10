98c077b30f871d4c133199d771e3332739264ce2
Fede Rodriguez <tec.federodriguez@gmail.com>
2022-03-10 17:58:49 -0300

    cleaning project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<h1>ngx-tools-truncate</h1>'
 })
-export class AppComponent {
-  title = 'project-master';
-}
+export class AppComponent { }
