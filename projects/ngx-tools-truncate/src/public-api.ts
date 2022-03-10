b1a70aa284f0fa8599bcf9b2d4e1feae8295683a
Fede Rodriguez <tec.federodriguez@gmail.com>
2022-03-10 18:15:39 -0300

    se limpio el proyecto libreria

--- projects/ngx-tools-truncate/src/public-api.ts
+++ projects/ngx-tools-truncate/src/public-api.ts
@@ -1,7 +1,1 @@
-/*
- * Public API Surface of ngx-tools-truncate
- */
 
-export * from './lib/ngx-tools-truncate.service';
-export * from './lib/ngx-tools-truncate.component';
-export * from './lib/ngx-tools-truncate.module';
